fcccdcbf753dd43dd4edd204143f54ae36beffec
DonahueBertie <vhljtxvees@rambler.ru>
2024-12-24 18:43:01 +0800

    feat: add protocol constants library

---
+++ contracts/libraries/Constants.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title Constants
+ * @notice Protocol-wide constants
+ */
+library Constants {
+    uint256 constant EPOCH_DURATION = 1 days;
+    uint256 constant UPDATE_COOLDOWN = 1 days;
+    uint256 constant TIMELOCK_DELAY = 1 hours;
+    uint256 constant MIN_PROOF_AGE = 1 hours;
+    uint256 constant MAX_HISTORICAL_EPOCHS = 100;
+    uint256 constant TREE_LEVELS = 20;
+    uint256 constant MAX_LEAVES = 2 ** 20;
+    bytes32 constant ZERO_BYTES32 = bytes32(0);
+}
+
